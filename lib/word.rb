519ae0fdb9ada524cfb97b52592e7c2d753cb513
Guy Halperin & Sergii Mertsalov <student@epicodus.com>
2014-07-31 12:17:53 -0700

    add duplicate letter functionality

--- lib/word.rb
+++ lib/word.rb
@@ -3,12 +3,18 @@ class Word
   attr_accessor :word
 
   def initialize(word)
+    @status = ""
     @word = word
-    @word_options = ["cat", "dog", "whale"]
+    @correct = []
+    @incorrect = []
+  end
+
+  def split_word
+    @split_word = @word.split("")
   end
 
   def hide_word
-   split_word
+    split_word
     word_with_stars = ''
     split_word.each do |x|
       word_with_stars << '*'
@@ -16,11 +22,38 @@ class Word
     word_with_stars
   end
 
-  def split_word
-    split_word = @word.split("")
+  def current_status
+    @status = hide_word
   end
 
-end
+  def letter_in_word(guessed_char)
+    if word.include?(guessed_char)
+      @correct << guessed_char
+    else
+      @incorrect << guessed_char
+    end
+  end
 
+  def display_word(guessed_letter)
+    self.letter_in_word(guessed_letter)
+    display_word = ""
+    split_word.each do |letter|
+      if @correct.include?(letter)
+        display_word += letter
+      else
+        display_word += '*'
+      end
+    end
+    @status = display_word
+  end
+end
 word = Word.new('apple')
-p word.hide_word
+
+# word.letter_in_word("p")
+# word.letter_in_word("e")
+# word.letter_in_word("a")
+# word.letter_in_word('s')
+# word.letter_in_word('r')
+word.current_status
+p word.display_word('a')
+p word.display_word('p')
--- spec/hangman_spec.rb
+++ spec/hangman_spec.rb
@@ -2,10 +2,18 @@ require 'rspec'
 require 'word'
 
 describe 'Word' do
-  it 'takes a word and replaces it with underscores' do
+  it 'takes a word and replaces it with stars' do
     test_word = Word.new("apple")
     expect(test_word.hide_word).to eq "*****"
   end
 
-  it ''
+  it 'takes a guessed letter and searches in the word for a match and replaces the hidden word with the matched letters' do
+    test_word = Word.new("pear")
+    expect(test_word.display_word('p')).to eq "p***"
+  end
+
+  it 'handles duplicate letter matches' do
+    test_word = Word.new("apple")
+    expect(test_word.display_word('p')).to eq "*pp**"
+  end
 end
